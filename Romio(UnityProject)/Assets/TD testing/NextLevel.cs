245c22b9ad1d54085e2fd5b14208e2225fe28b68
Thomas Davis <21.thomas.davis@ksd.org>
2020-01-09 13:02:38 -0800

    got level to level transfers working you will need to tag the  objects depending on which level you want to go to

---
+++ Romio(UnityProject)/Assets/TD testing/NextLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "TLTD")
+        {
+            SceneManager.LoadScene("TD testing 2");
+        }
+        if(collision.gameObject.tag == "NL2")
+        {
+            SceneManager.LoadScene("Level 2");
+        }
+        if(collision.gameObject.tag == "NL3")
+        {
+            SceneManager.LoadScene("Level 3");
+        }
+        if(collision.gameObject.tag == "SA")
+        {
+            SceneManager.LoadScene("Shoping area");
+        }
+    }
+}
