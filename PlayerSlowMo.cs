1faed6aaf58a5972e6ecee327757a5f95cd5cd13
JohnMBurnside <51937396+JohnMBurnside@users.noreply.github.com>
2020-01-16 10:21:37 -0800

    Create PlayerSlowMo.cs

---
+++ PlayerSlowMo.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerSlowMo : MonoBehaviour
+{
+  #region VARIABLES
+  [Header("Slow Mo Settings")]
+  public float slowMoMaxTime = 8f;
+  public float slowMoTime;
+  public Slider slowMoSlider;
+  public bool slowMoOn;
+  #endregion
+  //UNITY FUNCTIONS
+  #region START FUNCTION
+  void Start()
+  {
+      slowMoTime = slowMoMaxTime = 8f;
+      slowMoSlider.maxValue = slowMoMaxTime;
+      slowMoSlider.value = slowMoTime;
+  }
+  #endregion
+  #region UPDATE FUNCTION
+  void Update()
+  {
+    //Slow-Mo
+    if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        SlowMoSwitch();
+    if(slowMoOn == true)
+    {
+        slowMoTime -= Time.deltaTime;
+        if(slowMoTime < 0.1)
+            SlowMoSwitch();
+    }
+    else if(slowMoOn == false && slowMoTime < slowMoMaxTime)
+        slowMoTime += Time.deltaTime;
+    //Slow Mo Slider
+    slowMoSlider.value = slowMoTime;
+  }
+  #endregion
+  //PLAYER SLOW MO FUNCTIONS
+  #region SLOW MO SWITCH FUNCTION
+  void SlowMoSwitch()
+  {
+      if(slowMoOn == false)
+      {
+          Time.timeScale = 0.6f;
+          slowMoOn = true;
+      }
+      else
+      {
+          Time.timeScale = 1;
+          slowMoOn = false;
+      }
+  }
+  #endregion
+}
