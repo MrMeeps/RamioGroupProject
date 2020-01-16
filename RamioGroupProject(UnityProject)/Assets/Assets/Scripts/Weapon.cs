9e0315b8d3b40f93dc399e3ac69d5257b7029f75
Thomas Davis <21.thomas.davis@ksd.org>
2020-01-16 13:51:15 -0800

    made changes to new level 1

---
+++ RamioGroupProject(UnityProject)/Assets/Assets/Scripts/Weapon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon")]
+public class Weapon : ScriptableObject
+{
+    #region VARIABLES
+    public enum Weapons {sword, bow}
+    public Weapons      weaponType;
+    public string       weaponName;
+    public int          damage;
+    #endregion
+}
